05b7d449d0941a9f9a462b5f46561d2c32049473
Farahad Khurami <fkhurami98@gmail.com>
2024-08-26 16:40:28 +0100

    changed logic to use last available price instead of fixed days + other changes

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,5 +1,5 @@
+from datetime import datetime
 from logging import getLogger, INFO
-from datetime import datetime, timedelta
 import urllib3
 import json
 from config import PORTFOLIO, EODHD_API_KEY, WEBHOOK_URL
@@ -8,24 +8,22 @@ logger = getLogger()
 logger.setLevel(INFO)
 
 
-def get_eod_adjusted_close_price(asset_ticker, asset_name) -> float:
-    today = datetime.today().strftime("%Y-%m-%d")
-    yesterday = (datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d")
-
+def get_eod_adj_close_price(asset_ticker, asset_name):
     try:
         http = urllib3.PoolManager()
-        url = f"https://eodhd.com/api/eod/{asset_ticker}?api_token={EODHD_API_KEY}&fmt=json&from={yesterday}&to={today}"
+        url = f"https://eodhd.com/api/eod/{asset_ticker}?api_token={EODHD_API_KEY}&fmt=json&order=d"
         response = http.request("GET", url)
         data = json.loads(response.data.decode("utf-8"))
-        adjusted_close_price = data[0]["adjusted_close"]
-        logger.info(
-            f"Fetched adjusted close price for {asset_name}: £{adjusted_close_price}"
-        )
+
+        adj_close_price = data[0]["adjusted_close"]
+        adj_close_date = data[0]["date"]
+
+        logger.info(f"Adjusted close price for {asset_name}: £{adj_close_price}")
     except Exception as e:
         logger.error(f"Error fetching data for {asset_name}: {e}")
         return 0.0
 
-    return adjusted_close_price
+    return adj_close_price, adj_close_date
 
 
 def calculate_portfolio_value() -> float:
@@ -40,7 +38,7 @@ def calculate_portfolio_value() -> float:
             logger.info(f"Adding cash to total value: £{asset_quantity}")
             total_value += asset_quantity
         else:
-            adjusted_close_price = get_eod_adjusted_close_price(
+            adjusted_close_price, adjusted_close_price_date = get_eod_adj_close_price(
                 asset_ticker, asset_name
             )
             if adjusted_close_price == 0.0:
@@ -53,7 +51,7 @@ def calculate_portfolio_value() -> float:
             total_value += total_asset_value
 
     logger.info(f"Total portfolio value calculated: {total_value:,.2f}")
-    return total_value
+    return total_value, adjusted_close_price_date
 
 
 def send_webhook_message(message) -> int:
@@ -77,10 +75,11 @@ def send_webhook_message(message) -> int:
 
 
 def lambda_handler(event, context):
-    logger.info("Lambda function invoked.")
-    total_value = calculate_portfolio_value()
-    formatted_value = f"£{total_value:,.2f}"
-    message = f"Total Portfolio Value: {formatted_value}"
+    total_value, date_time = calculate_portfolio_value()
+
+    fmtd_total_value = f"£{total_value:,.2f}"
+    fmtd_date_time = datetime.strptime(date_time, "%Y-%m-%d").strftime("%d %B %Y")
+
+    message = f"Total Value: {fmtd_total_value}\nLast Close: {fmtd_date_time}"
 
     send_webhook_message(message)
-    logger.info("Lambda function completed.")
